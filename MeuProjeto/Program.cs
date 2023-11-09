74f2d9b43ece711c29e026782f483de781805942
tacruz33 <talesdearaujo18@gmail.com>
2023-11-09 17:21:01 -0300

    Atualziando os comandos

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+#region 
+
+int tipoInteiro;
+double tipoDouble;
+string tipoString;
+long tipoLong;
+
+tipoInteiro = int.MaxValue;
+tipoLong = long.MaxValue;
+
+tipoDouble = tipoInteiro = tipoLong;
+
+tipoString = "100A";
+
+tipoInteiro = int.Parse(tipoString);
+
+Console.WriteLine("O máximo inteiro é : " + tipoInteiro);
+Console.WriteLine("O máximo long é : " + tipoLong);
+
+#endregion
