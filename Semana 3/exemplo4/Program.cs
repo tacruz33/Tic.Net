ca2799edf69393934ed891fc509b177e53041b3e
Tales <talesdearaujo18@gmail.com>
2023-11-23 17:38:47 -0300

    Aula 23-11

---
+++ Semana 3/exemplo4/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+
+class Combinacoes
+{
+    static void Main()
+    {
+    
+        List<Tuple<string, int>> combine = new List<Tuple<string, int>>
+        {
+            Tuple.Create("Tales",175);
+            Tuple.Create("Beatriz",123);
+            Tuple.Create("Ana",146);
+            Tuple.Create("Carol",183);
+           Console.WriteLine(tupla.Item1); 
+        }
+    
+
+        
+
+}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+    
+        List<Tuple<string, int>> pessoas = new List<Tuple<string, int>>
+        {
+            Tuple.Create("Tales", )
+            Tuple.Create("Maria", 160),
+            Tuple.Create("Carlos", 180),
+            Tuple.Create("Ana", 165),
+            Tuple.Create("Pedro", 175)
+        };
+
+}
